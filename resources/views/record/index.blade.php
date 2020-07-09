8da3fe4032119d7d777a068e1421b8c4c3af838b
Daniel Pett <dejp3@cam.ac.uk>
2020-07-09 18:58:42 +0100

    Amend template for missing data

--- resources/views/record/index.blade.php
+++ resources/views/record/index.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts/layout')
 
 @section('hero_image_title', "The inside of our Founder's entrance")
-@section('title','Objects and artworks')
 
 @section('content')
 <div class="row">
@@ -26,33 +25,52 @@
       </div>
     </div>
     @endif
-    
+
     <!-- <pre>@php(var_dump($record['_source']))</pre> -->
     <div class="col mb-3">
       <div class="card card-body h-100">
         <div class="container h-100">
-          <h3>
-            {{ ucfirst($record['_source']['summary_title']) }}
-          </h3>
+            @section('title', ucfirst($record['_source']['summary_title']))
 
           @if(array_key_exists('description', $record['_source']))
-          <p>{{ ucfirst($record['_source']['description'][0]['value']) }} </p>
+            <p>{{ ucfirst($record['_source']['description'][0]['value']) }} </p>
+          @endif
+
+          @if(array_key_exists('note', $record['_source']))
+            <p>{{ ucfirst($record['_source']['note'][0]['value']) }} </p>
+          @endif
+
+          @if(array_key_exists('legal', $record['_source']))
+          <h4>Legal notes</h4>
+            <p>{{ ucfirst($record['_source']['legal']['credit_line']) }} </p>
           @endif
 
           @if(array_key_exists('creation', $record['_source']['lifecycle']))
           <h4>Dating</h4>
           <ul>
+            @if(array_key_exists('periods', $record['_source']['lifecycle']['creation'][0]))
             @foreach($record['_source']['lifecycle']['creation'][0]['periods'] as $date)
-            <li>{{ $date['summary_title']}}</li>
+              <li>{{ $date['summary_title']}}</li>
             @endforeach
+            @endif
+            @if(array_key_exists('date', $record['_source']['lifecycle']['creation'][0]))
+              <li>Date: {{$record['_source']['lifecycle']['creation'][0]['date'][0]['value']}}</li>
+            @endif
           </ul>
+
+            @if(array_key_exists('maker', $record['_source']['lifecycle']['creation'][0]))
+            <h4>Production</h4>
+            <ul>
+              <li>Made by: {{ $record['_source']['lifecycle']['creation'][0]['maker'][0]['summary_title']}}</li>
+            </ul>
+            @endif
           @endif
 
           @if(array_key_exists('measurements', $record['_source']))
           <h4>Measurements and weight</h4>
           <ul>
             @foreach($record['_source']['measurements']['dimensions'] as $dim)
-            <li>{{ $dim['value'] }}</li>
+              <li>{{ $dim['value'] }}</li>
             @endforeach
           </ul>
           @endif
@@ -61,26 +79,26 @@
 
           @if(array_key_exists('acquisition', $record['_source']['lifecycle']))
           <h4>Acquisition and important dates</h4>
-          <ul>
-            <li>Acquired: {{ $record['_source']['lifecycle']['acquisition'][0]['method']['value'] }} {{ $record['_source']['lifecycle']['acquisition'][0]['date'][0]['value'] }}</li>
-          </ul>
+            <ul>
+              <li>Method of acquisition: {{ $record['_source']['lifecycle']['acquisition'][0]['method']['value'] }} {{ $record['_source']['lifecycle']['acquisition'][0]['date'][0]['value'] }}</li>
+            </ul>
           @endif
 
           @if(array_key_exists('publications', $record['_source']))
-
           <h4>References and bibliographic entries</h4>
-          <ul>
-            @foreach($record['_source']['publications'] as $pub)
-            <li>{{ $pub['summary_title'] }}</li>
-            @endforeach
-          </ul>
+            <ul>
+              @foreach($record['_source']['publications'] as $pub)
+                <li>{{ $pub['summary_title'] }}</li>
+              @endforeach
+            </ul>
           @endif
 
           <h4>Identification numbers</h4>
-          <ul>
-            <li>Accession number:   {{ $record['_source']['identifier'][0]['accession_number'] }}</li>
-            <li>Primary reference:   {{ $record['_source']['identifier'][1]['priref'] }}</li>
-          </ul>
+            <ul>
+              @foreach($record['_source']['identifier'] as $id)
+                <li>{{ ucfirst($id['type']) }}: {{ $id['value']}}</li>
+              @endforeach
+            </ul>
         </div>
       </div>
     </div>
