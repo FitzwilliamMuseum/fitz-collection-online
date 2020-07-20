cd042d6f57033d7807db3dfe6f4bc97e9e23130a
Daniel Pett <dejp3@cam.ac.uk>
2020-07-20 20:50:10 +0100

    Centred image

--- resources/views/includes/elements/media.blade.php
+++ resources/views/includes/elements/media.blade.php
@@ -15,17 +15,17 @@
   @if(array_key_exists('multimedia', $record['_source']))
   @if(!empty(array_slice($record['_source']['multimedia'],1)))
   <h3>Alternative views</h3>
-  <div class="shadow-sm p-3 mx-auto mb-3 mt-3 rounded">
-    <div class="row">
+  <div class="row ">
       @foreach(array_slice($record['_source']['multimedia'],1) as $media)
-      <div class="col-md-4 mx-auto">
-        <a href="/id/image/{{ $media['admin']['id']}}"><img class="img-fluid  mt-4" src="https://api.fitz.ms/mediaLib/{{ $media['processed']['preview']['location'] }}"
+      <div class="col-md-4 mt-3">
+        <div class="card card-body h-100">
+        <a href="/id/image/{{ $media['admin']['id']}}"><img class="img-fluid mx-auto d-block" src="https://api.fitz.ms/mediaLib/{{ $media['processed']['preview']['location'] }}"
         loading="lazy" alt="An image of {{ ucfirst($record['_source']['summary_title']) }}"
         /></a>
       </div>
+    </div>
       @endforeach
     </div>
-  </div>
   @endif
 </div>
 
