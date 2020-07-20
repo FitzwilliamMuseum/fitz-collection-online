0a8988fea3fbf86ce7ae97c4ae0ee5c9aa358505
Daniel Pett <dejp3@cam.ac.uk>
2020-07-20 14:53:39 +0100

    Add materials

--- resources/views/includes/elements/materials.blade.php
+++ resources/views/includes/elements/materials.blade.php
@@ -3,7 +3,9 @@
 <ul>
   @foreach($record['_source']['materials'] as $material)
     @foreach($material as $mat)
-        <li><a href="/id/terminology/{{ $material['reference']['admin']['id']}}">{{ ucfirst($material['reference']['summary_title'])}}</a></li>
+      @if(array_key_exists('admin', $mat))
+      <li><a href="/id/terminology/{{ $mat['admin']['id']}}">{{ ucfirst($mat['summary_title'])}}</a></li>
+      @endif
     @endforeach
   @endforeach
 </ul>
