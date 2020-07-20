60331c65083500479bd028e1fe55a6e12ae2bb1d
Daniel Pett <dejp3@cam.ac.uk>
2020-07-20 08:16:44 +0100

    Amend css

--- resources/views/includes/structure/iiif.blade.php
+++ resources/views/includes/structure/iiif.blade.php
@@ -2,7 +2,7 @@
 @if(array_key_exists('zoom', $record["_source"]["multimedia"][0]["processed"]))
 <script type="text/javascript" src="/js/mootools-core-1.6.0-compressed.js"></script>
 <script type="text/javascript" src="/js/iipmooviewer-2.0-min.js"></script>
-<link rel="stylesheet" type="text/css" media="all" href="/css/iip.min.css" />
+<link rel="stylesheet" type="text/css" media="all" href="/css/iip.css" />
 <script type="text/javascript">
 
 // IIPMooViewer options: See documentation at http://iipimage.sourceforge.net for more details
@@ -20,9 +20,6 @@ new IIPMooViewer( "viewer", {
   image: image,
   credit: credit,
   server: '/iipsrv/iipsrv.fcgi',
-  viewport: {
-    resolution:3
-  }
 });
 
 </script>
