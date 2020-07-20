551707b8a3a3d6f5e3bbcdf534310e368259483e
Daniel Pett <dejp3@cam.ac.uk>
2020-07-20 14:28:48 +0100

    Amend

--- resources/views/includes/elements/materials.blade.php
+++ resources/views/includes/elements/materials.blade.php
@@ -3,9 +3,8 @@
 <ul>
   @foreach($record['_source']['materials'] as $material)
     @foreach($material as $mat)
-        <li><a href="/id/terminology/{{ $material['admin']['id']}}">{{ ucfirst($material['summary_title'])}}</a></li>
+        <li><a href="/id/terminology/{{ $material['reference']['admin']['id']}}">{{ ucfirst($material['reference']['summary_title'])}}</a></li>
     @endforeach
-
   @endforeach
 </ul>
 @endif
