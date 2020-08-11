ce92409d5499d7b045820834954e682dd01ee295
Daniel Pett <dejp3@cam.ac.uk>
2020-08-11 23:59:05 +0100

    Added csrf

--- resources/views/includes/structure/meta.blade.php
+++ resources/views/includes/structure/meta.blade.php
@@ -4,9 +4,9 @@
 
     <title>The Fitzwilliam Museum - @yield('title')</title>
     <meta name="description" content="@yield('description')" />
-    @hasSection('keywords')
-      <meta name="keywords" content="@yield('keywords')" />
-    @endif
+@hasSection('keywords')
+  <meta name="keywords" content="@yield('keywords')" />
+@endif
     <!-- Canonical link -->
     <link rel="canonical" href="{{ URL::current() }}" />
 
@@ -18,13 +18,15 @@
     <meta property="og:url" content="{{ URL::current() }}" />
     <meta property="og:site_name" content="The Fitzwilliam Museum" />
 
-    @hasSection('hero_image')
-      <meta property="og:image" content="@yield('hero_image')" />
-      <meta name="twitter:image" content="@yield('hero_image')" />
-    @else
-      <meta property="og:image" content="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fitzwilliam-museum-main-entrance-2018_3.jpg" />
-      <meta name="twitter:image" content="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fitzwilliam-museum-main-entrance-2018_3.jpg" />
-    @endif
+@hasSection('hero_image')
+  <meta property="og:image" content="@yield('hero_image')" />
+  <meta name="twitter:image" content="@yield('hero_image')" />
+@else
+  <meta property="og:image" content="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fitzwilliam-museum-main-entrance-2018_3.jpg" />
+  <meta name="twitter:image" content="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/fitzwilliam-museum-main-entrance-2018_3.jpg" />
+@endif
+
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Twitter card -->
     <meta name="twitter:card" content="summary" />
