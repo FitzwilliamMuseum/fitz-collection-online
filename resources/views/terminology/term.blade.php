ece2c09f793f70226fb629b4f8de1d6fd2de34ab
Daniel Pett <dejp3@cam.ac.uk>
2020-08-14 15:56:04 +0100

    Update view

--- resources/views/terminology/term.blade.php
+++ resources/views/terminology/term.blade.php
@@ -10,33 +10,36 @@
       @foreach($term['_source']['description'] as $description)
       <h3>{{ ucfirst($description['type']) }}</h3>
       <p>
-      {{ ucfirst($description['value']) }}
+        {{ ucfirst($description['value']) }}
       </p>
       @endforeach
       @else
-      <p>No supplementary data available</p>
+        <p>
+          No supplementary data available
+        </p>
       @endif
       <p>This term has <strong>{{ number_format($count['count'])}}</strong> records attributed within our system.</p>
 
     </div>
 @endforeach
-<h3>Example connected records</h3>
+
+<h3>Example connected records (with images)</h3>
     <div class="row">
     @foreach($use['hits'] as $record)
     <div class="col-md-4 mb-3">
       <div class="card card-body h-100">
         <div class="container h-100">
           @if(array_key_exists('multimedia', $record['_source']))
-          <a href="/id/object/{{ $record['_source']['identifier'][1]['priref']}}"><img class="img-fluid" src="https://api.fitz.ms/mediaLib/{{ $record['_source']['multimedia'][0]['processed']['preview']['location'] }}"
-           loading="lazy" alt="An image of {{ ucfirst($record['_source']['summary_title']) }}"
-          /></a>
+            <a href="/id/object/{{ $record['_source']['identifier'][1]['priref']}}"><img class="img-fluid mx-auto d-block" src="https://api.fitz.ms/mediaLib/{{ $record['_source']['multimedia'][0]['processed']['preview']['location'] }}"
+             loading="lazy" alt="An image of {{ ucfirst($record['_source']['summary_title']) }}"
+            /></a>
           @else
-          <a href="/id/object/{{ $record['_source']['identifier'][1]['priref']}}"><img class="img-fluid" src="https://content.fitz.ms/fitz-website/assets/no-image-available.png?key=directus-large-crop"
-          alt="A stand in image for {{ ucfirst($record['_source']['summary_title']) }}}"/></a>
+            <a href="/id/object/{{ $record['_source']['identifier'][1]['priref']}}"><img class="img-fluid" src="https://content.fitz.ms/fitz-website/assets/no-image-available.png?key=directus-large-crop"
+            alt="A stand in image for {{ ucfirst($record['_source']['summary_title']) }}}"/></a>
           @endif
           <div class="contents-label mb-3">
             <h3>
-            <a href="/id/object/{{ $record['_source']['identifier'][1]['priref']}}">{{ ucfirst($record['_source']['summary_title']) }}</a>
+              <a href="/id/object/{{ $record['_source']['identifier'][1]['priref']}}">{{ ucfirst($record['_source']['summary_title']) }}</a>
             </h3>
             <p>
               @if(array_key_exists('department', $record['_source']))
@@ -52,5 +55,7 @@
     @endforeach
 
   </div>
-
+  <div class="container shadow-sm p-3 mx-auto mb-3 rounded">
+    <a href="/search/results?query=**" class="btn btn-dark btn-block">Find all results</a>
+  </div>
 @endsection
