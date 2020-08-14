c2bd4abe1f902e46ad21b9e7f509b8a1ff48ede9
Daniel Pett <dejp3@cam.ac.uk>
2020-08-14 15:55:28 +0100

    Add directives

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,26 +3,52 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use ImLiam\BladeHelper\Facades\BladeHelper;
+use Geocoder\Query\GeocodeQuery;
+use Illuminate\Support\Facades\Http;
+use Storage;
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+  * Register any application services.
+  *
+  * @return void
+  */
+  public function register()
+  {
+    //
+  }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+  /**
+  * Bootstrap any application services.
+  *
+  * @return void
+  */
+  public function boot()
+  {
+    BladeHelper::directive('fa', function(string $iconName, string $text = null, $classes = '') {
+      if (is_array($classes)) {
+        $classes = join(' ', $classes);
+      }
+
+      $text = $text ?? $iconName;
+
+      return "<i class='fab fa-{$iconName} {$classes}' aria-hidden='true' title='{$text}'></i><span class='sr-only'>{$text}</span>";
+    });
+
+    BladeHelper::directive('geocode', function(string $placestring) {
+
+    });
+
+    BladeHelper::directive('lookup_term', function( $term){
+        $json =  '[{"term": "term-14162","label": "Imperial (Roman)"},
+        {"term": "term-14163","label": "Imperial (Roman)"}]';
+        rtrim($json, "\0");
+        $arr = json_decode($json,true);
+        $filtered = array_filter($arr, function($object) use($term) {
+          return ($object['term'] === $term);
+        });
+       return($filtered[0]['label']);
+    });
+  }
 }
