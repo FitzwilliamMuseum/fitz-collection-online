b602854f0d25764d92fa352274da187509949965
Daniel Pett <dejp3@cam.ac.uk>
2020-08-17 09:52:22 +0100

    Fix agent cards

--- resources/views/agents/record.blade.php
+++ resources/views/agents/record.blade.php
@@ -19,14 +19,16 @@
       @foreach($use['hits'] as $record)
       <div class="col-md-4 mb-3">
         <div class="card  h-100">
+          <div class="embed-responsive embed-responsive-1by1">
           @if(array_key_exists('multimedia', $record['_source']))
-          <a href="/id/object/{{ $record['_source']['identifier'][1]['priref']}}"><img class="img-fluid" src="https://api.fitz.ms/mediaLib/{{ $record['_source']['multimedia'][0]['processed']['preview']['location'] }}"
+          <a href="/id/object/{{ $record['_source']['identifier'][1]['priref']}}"><img class="img-fluid embed-responsive-item" src="https://api.fitz.ms/mediaLib/{{ $record['_source']['multimedia'][0]['processed']['preview']['location'] }}"
            loading="lazy" alt="An image of {{ ucfirst($record['_source']['summary_title']) }}"
           /></a>
           @else
-          <a href="/id/object/{{ $record['_source']['identifier'][1]['priref']}}"><img class="img-fluid" src="https://content.fitz.ms/fitz-website/assets/no-image-available.png?key=directus-large-crop"
+          <a href="/id/object/{{ $record['_source']['identifier'][1]['priref']}}"><img class="img-fluid  embed-responsive-item" src="https://content.fitz.ms/fitz-website/assets/no-image-available.png?key=directus-large-crop"
           alt="A stand in image for {{ ucfirst($record['_source']['summary_title']) }}}"/></a>
           @endif
+        </div>
           <div class="card-body h-100">
             <div class="contents-label mb-3">
               <h3>
@@ -40,7 +42,6 @@
               </p>
             </div>
           </div>
-          <a href="/id/object/{{ $record['_source']['identifier'][1]['priref']}}" class="btn btn-dark">Read more</a>
         </div>
       </div>
       @endforeach
