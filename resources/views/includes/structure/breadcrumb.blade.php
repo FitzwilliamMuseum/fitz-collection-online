57a0f52fc7cdcbc340a4dc5d1981e49e73f6b263
Daniel Pett <dejp3@cam.ac.uk>
2020-08-18 20:05:18 +0100

    Fudge for url fragment

--- resources/views/includes/structure/breadcrumb.blade.php
+++ resources/views/includes/structure/breadcrumb.blade.php
@@ -4,7 +4,6 @@
   <ol class="breadcrumb">
     <li class="breadcrumb-item active" aria-current="page"><a href="{{ URL::to('https://beta.fitz.ms') }}">Home</a></li>
     <li class="breadcrumb-item active" aria-current="page"><a href="{{ URL::to('https://collection.beta.fitz.ms') }}">Objects and Artworks</a></li>
-
     @php
     $bread = URL::to('/');
     $link = Request::path();
@@ -12,19 +11,19 @@
     @endphp
 
     @if (Request::path() != '/')
-
-    @for($i = 0; $i < count($subs); $i++)
-    @php
-    $bread = $bread."/".$subs[$i];
-    $title = urldecode($subs[$i]);
-    $title = str_replace("-", " ", $title);
-    $title = ucwords($title);
-    @endphp
-    @if($title != 'Id')
-    <li class="breadcrumb-item active" aria-current="page">{{ $title }}</li>
-    @endif
+      @for($i = 0; $i < count($subs); $i++)
+        @php
+        $bread = $bread."/".$subs[$i];
+        $title = urldecode($subs[$i]);
+        $title = ucwords($title);
+        if(is_numeric($title)){
+          $title = $data[0]['_source']['identifier'][0]['accession_number'];
+        }
+        @endphp
+        @if($title != 'Id')
+        <li class="breadcrumb-item active" aria-current="page">{{ $title }}</li>
+        @endif
       @endfor
-
-      @endif
-    </ol>
-  </nav>
+   @endif
+  </ol>
+</nav>
