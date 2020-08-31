5a207de69398b4594a83f44b281f4e361a0d3b17
Daniel Pett <dejp3@cam.ac.uk>
2020-08-31 09:21:42 +0100

    Added redis config

--- config/cache.php
+++ config/cache.php
@@ -73,8 +73,23 @@ return [
         ],
 
         'redis' => [
-            'driver' => 'redis',
-            'connection' => 'cache',
+
+            'client' => env('REDIS_CLIENT', 'predis'),
+
+            'default' => [
+              'host' => env('REDIS_HOST', '127.0.0.1'),
+              'password' => env('REDIS_PASSWORD', null),
+              'port' => env('REDIS_PORT', 6379),
+              'database' => env('REDIS_DB', 0),
+            ],
+
+            'cache' => [
+              'host' => env('REDIS_HOST', '127.0.0.1'),
+              'password' => env('REDIS_PASSWORD', null),
+              'port' => env('REDIS_PORT', 6379),
+              'database' => env('REDIS_CACHE_DB', 1),
+            ],
+
         ],
 
         'dynamodb' => [
