3ba1fd551b51a591361238b772343750372e4e2a
Daniel Pett <dejp3@cam.ac.uk>
2020-10-19 23:15:35 +0100

    Add download

--- resources/views/includes/elements/media.blade.php
+++ resources/views/includes/elements/media.blade.php
@@ -1,36 +1,40 @@
 @if(array_key_exists('multimedia', $record['_source']))
-<div class="col-md-12 mb-3">
-  <div class="shadow-sm p-3 mx-auto mb-3 mt-3 rounded">
-    <div>
-      @if(array_key_exists('multimedia', $record['_source']))
-      <a href="/id/image/{{ $record['_source']['multimedia'][0]['admin']['id']}}"><img class="img-fluid mx-auto d-block" src="https://api.fitz.ms/mediaLib/{{ $record['_source']['multimedia'][0]['processed']['original']['location'] }}"
-      loading="lazy" alt="An image of {{ ucfirst($record['_source']['summary_title']) }}"
-      /></a>
-      @endif
-      <span class="btn btn-wine m-1 p-2 share">
-          <a href="https://api.fitz.ms/mediaLib/{{ $record['_source']['multimedia'][0]['processed']['original']['location'] }}" target="_blank"
-           download="{{ basename($record['_source']['multimedia'][0]['processed']['original']['location'] ) }}"><i class="fas fa-download mr-2"></i>  Download this image</a>
-        </span>
-    </div>
-  </div>
-  @endif
-
-  @if(array_key_exists('multimedia', $record['_source']))
-  @if(!empty(array_slice($record['_source']['multimedia'],1)))
-  <h3>Alternative views</h3>
-  <div class="row ">
-      @foreach(array_slice($record['_source']['multimedia'],1) as $media)
-      <div class="col-md-4 mt-3">
-        <div class="card card-body h-100">
-        <a href="/id/image/{{ $media['admin']['id']}}"><img class="img-fluid mx-auto d-block" src="https://api.fitz.ms/mediaLib/{{ $media['processed']['preview']['location'] }}"
-        loading="lazy" alt="An image of {{ ucfirst($record['_source']['summary_title']) }}"
-        /></a>
+  <div class="col-md-12 mb-3">
+    <div class="shadow-sm p-3 mx-auto mb-3 mt-3 rounded">
+      <div>
+        @if(array_key_exists('multimedia', $record['_source']))
+          <a href="/id/image/{{ $record['_source']['multimedia'][0]['admin']['id']}}"><img class="img-fluid mx-auto d-block" src="https://api.fitz.ms/mediaLib/{{ $record['_source']['multimedia'][0]['processed']['original']['location'] }}"
+            loading="lazy" alt="An image of {{ ucfirst($record['_source']['summary_title']) }}"
+            /></a>
+          @endif
+          <span class="btn btn-wine m-1 p-2 share">
+            <a href="https://api.fitz.ms/mediaLib/{{ $record['_source']['multimedia'][0]['processed']['original']['location'] }}" target="_blank"
+            download="{{ basename($record['_source']['multimedia'][0]['processed']['original']['location'] ) }}"><i class="fas fa-download mr-2"></i>  Download this image</a>
+          </span>
+        </div>
       </div>
-    </div>
-      @endforeach
-    </div>
-  @endif
-</div>
+    @endif
 
-{{-- @include('includes/structure/iiif') --}}
-@endif
+    @if(array_key_exists('multimedia', $record['_source']))
+      @if(!empty(array_slice($record['_source']['multimedia'],1)))
+        <h3>Alternative views</h3>
+        <div class="row ">
+          @foreach(array_slice($record['_source']['multimedia'],1) as $media)
+            <div class="col-md-4 mt-3">
+              <div class="card card-body h-100">
+                <a href="/id/image/{{ $media['admin']['id']}}"><img class="img-fluid mx-auto d-block" src="https://api.fitz.ms/mediaLib/{{ $media['processed']['preview']['location'] }}"
+                  loading="lazy" alt="An image of {{ ucfirst($record['_source']['summary_title']) }}"
+                  /></a>
+                  <span class="btn btn-wine m-1 p-2 share">
+                    <a href="https://api.fitz.ms/mediaLib/{{ $media['processed']['original']['location'] }}" target="_blank"
+                    download="{{ basename($media['processed']['original']['location'] ) }}"><i class="fas fa-download mr-2"></i>  Download this image</a>
+                  </span>
+                </div>
+              </div>
+            @endforeach
+          </div>
+        @endif
+      </div>
+
+      {{-- @include('includes/structure/iiif') --}}
+    @endif
