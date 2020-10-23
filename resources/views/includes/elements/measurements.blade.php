198b93fa3b5aef51fbdec870360728e300355cf0
Daniel Pett <dejp3@cam.ac.uk>
2020-10-23 09:56:12 +0100

    Push new dims

--- resources/views/includes/elements/measurements.blade.php
+++ resources/views/includes/elements/measurements.blade.php
@@ -1,11 +1,22 @@
 @if(array_key_exists('measurements', $record['_source']))
 <h4>Measurements and weight</h4>
-<div class="alert alert-dark" role="alert">
-  <p>At the moment, our system does not display units or type of measurements below. We will rectify this as soon as possible.</p>
-</div>
+
 <ul>
   @foreach($record['_source']['measurements']['dimensions'] as $dim)
-  <li>{{ $dim['value'] }}</li>
+    @if(array_key_exists('dimension',$dim) && array_key_exists('units',$dim))
+      <li>{{ $dim['dimension'] }}: {{ $dim['value'] }} {{  $dim['units'] }}</li>
+    @else
+      @section('dims-message')
+        <div class="alert alert-dark" role="alert">
+          <p>
+            At the moment, this record does not display units or type of
+            measurements. We will rectify this as soon as possible.
+          </p>
+        </div>
+      @endsection
+        <li>{{ $dim['value'] }}</li>
+    @endif
   @endforeach
 </ul>
 @endif
+@yield('dims-message')
