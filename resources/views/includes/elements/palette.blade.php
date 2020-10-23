666d032612b74c4dd41686e9870911d28ce96cf1
Daniel Pett <dejp3@cam.ac.uk>
2020-10-23 15:16:49 +0100

    Center colours

--- resources/views/includes/elements/palette.blade.php
+++ resources/views/includes/elements/palette.blade.php
@@ -1,6 +1,6 @@
 <h4>Colours in this image</h4>
 <div <div class="col-12 shadow-sm p-3 mx-auto mb-3 rounded">
-  <div class="swatches">
+  <div class="swatches text-center">
     @foreach ($palette as $color)
       <div class="swatch mr-2" style="background-color: rgb({{ $color[0] }}, {{ $color[1] }}, {{ $color[2] }})"></div>
     @endforeach
