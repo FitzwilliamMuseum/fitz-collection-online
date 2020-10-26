173bb285818af5ee18d8ae1bd706da0918aa4de0
Daniel Pett <dejp3@cam.ac.uk>
2020-10-26 13:14:36 +0000

    Fix naming keys

--- resources/views/includes/elements/name.blade.php
+++ resources/views/includes/elements/name.blade.php
@@ -2,8 +2,15 @@
   <h4>Named entities</h4>
   <ul>
   @foreach ($record['_source']['name'] as $name)
+    @if(array_key_exists('reference', $name))
     <li>
-      <a href="{{ URL::to('/id/terminology/'. $name['reference']['admin']['id']) }}">{{ ucfirst($name['reference']['summary_title']) }}</a></li>
+      <a href="{{ URL::to('/id/terminology/' . $name['reference']['admin']['id']) }}">{{ ucfirst($name['reference']['summary_title']) }}</a>
+    </li>
+    @else
+      <li>
+        {{ ucfirst($name['value']) }}
+      </li>
+    @endif
   @endforeach
   </ul>
 @endif
