6d92324a35f0640e681f4739efaa9ed820dd135f
Daniel Pett <dejp3@cam.ac.uk>
2020-10-26 21:10:21 +0000

    Add titles

--- resources/views/includes/elements/descriptive.blade.php
+++ resources/views/includes/elements/descriptive.blade.php
@@ -1,11 +1,25 @@
 @section('title', ucfirst($record['_source']['summary_title']))
 
+@if(array_key_exists('title', $record['_source']))
+
+  <h4>
+    Titles
+  </h4>
+  <ul>
+  @foreach($record['_source']['title'] as $titles)
+  <li>{{ $titles['value'] }} </li>
+  @endforeach
+  </ul>
+@endif
+
 @if(array_key_exists('description', $record['_source']))
-@foreach($record['_source']['description'] as $description)
-  <p>{!! ucfirst(nl2br($description['value'])) !!} </p>
-@endforeach
+  @foreach($record['_source']['description'] as $description)
+    <p>{!! ucfirst(nl2br($description['value'])) !!} </p>
+  @endforeach
 @endif
 
 @if(array_key_exists('note', $record['_source']))
-<p>{{ ucfirst($record['_source']['note'][0]['value']) }} </p>
+  <p>
+    {{ ucfirst($record['_source']['note'][0]['value']) }}
+  </p>
 @endif
