e6caa0c218c137b5ea7038fd8b6001d20784f044
Daniel Pett <dejp3@cam.ac.uk>
2020-10-26 21:18:15 +0000

    Add agents/subjects back in

--- resources/views/includes/elements/agents-subjects.blade.php
+++ resources/views/includes/elements/agents-subjects.blade.php
@@ -1,20 +1,26 @@
-@if(array_key_exists('content',$record['_source']))
 
-  @if(array_key_exists('agents', $record['_source']['content']))
+  @if(array_key_exists('agents', $record['_source']))
   <h4>Agents depicted</h4>
   <ul>
-  @foreach($record['_source']['content']['agents'] as $agent)
-    <li><a href="/id/agent/{{ $agent['admin']['id']}}">{{ ucfirst($agent['summary_title'])}}</a></li>
+  @foreach($record['_source']['agents'] as $agent)
+    @if(array_key_exists('admin', $agent))
+      <li><a href="/id/agent/{{ $agent['admin']['id']}}">{{ ucfirst($agent['summary_title'])}}</a></li>
+    @else
+      <li>{{ ucfirst($agent['summary_title'])}}</li>
+    @endif
   @endforeach
   </ul>
   @endif
 
-  @if(array_key_exists('subjects', $record['_source']['content']))
+  @if(array_key_exists('subjects', $record['_source']))
   <h4>Subjects depicted</h4>
   <ul>
-  @foreach($record['_source']['content']['subjects'] as $subject)
-    <li><a href="/id/terminology/{{ $subject['admin']['id']}}">{{ ucfirst($subject['summary_title'])}}</a></li>
+  @foreach($record['_source']['subjects'] as $subject)
+    @if(array_key_exists('admin', $subject))
+      <li><a href="/id/terminology/{{ $subject['admin']['id']}}">{{ ucfirst($subject['summary_title'])}}</a></li>
+    @else
+      <li>{{ ucfirst($subject['summary_title'])}}</li>
+    @endif
   @endforeach
   </ul>
   @endif
-@endif
