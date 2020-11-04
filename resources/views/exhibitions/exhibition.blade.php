079cda7fdb92530363ca85b03171bbcaa1b54f4f
Daniel Pett <dejp3@cam.ac.uk>
2020-11-04 23:09:31 +0000

    Array key test for venues

--- resources/views/exhibitions/exhibition.blade.php
+++ resources/views/exhibitions/exhibition.blade.php
@@ -4,21 +4,27 @@
 @section('title', $exhibition['summary_title'])
 @section('content')
   <div class="shadow-sm p-3 mx-auto mb-3 rounded">
-    @foreach($exhibition['venues'] as $venue)
-      <p>Held: {{ $venue['summary_title'] }}</p>
-      @if(array_key_exists('@link', $venue))
-        @if(array_key_exists('date', $venue['@link']))
-          @foreach ($venue['@link']['date'] as $date)
-            @if(array_key_exists('from', $date))
-              From: {{ $date['from']['value'] }}
-            @endif
-            @if(array_key_exists('to', $date))
-              To: {{ $date['to']['value'] }}
-            @endif
-          @endforeach
+    @if(array_key_exists('venues', $exhibition))
+      @foreach($exhibition['venues'] as $venue)
+        <p>Held: {{ $venue['summary_title'] }}</p>
+        @if(array_key_exists('@link', $venue))
+          @if(array_key_exists('date', $venue['@link']))
+            @foreach ($venue['@link']['date'] as $date)
+              @if(array_key_exists('from', $date))
+                From: {{ $date['from']['value'] }}
+              @endif
+              @if(array_key_exists('to', $date))
+                To: {{ $date['to']['value'] }}
+              @endif
+            @endforeach
+          @endif
         @endif
+        <ul>
+        @endforeach
+      @else
+        <p>
+          No details recorded.
+        </p>
       @endif
-      <ul>
-    @endforeach
-  </div>
-@endsection
+    </div>
+  @endsection
