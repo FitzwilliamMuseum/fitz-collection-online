1f1bb1ba3d59c90781bae19a710a5f9e77ef88c3
Daniel Pett <dejp3@cam.ac.uk>
2021-03-03 09:59:35 +0000

    Update download file name

--- resources/views/includes/elements/media-single.blade.php
+++ resources/views/includes/elements/media-single.blade.php
@@ -8,7 +8,7 @@
   <div class="text-center mb-2 mt-2">
     <span class="btn btn-wine m-1 p-2 share">
       <a href="{{ env('APP_URL')}}/imagestore/{{ $record['processed']['original']['location'] }}" target="_blank"
-      download="{{ basename($object['multimedia'][0]['processed']['original']['location'] ) }}"><i class="fas fa-download mr-2"></i>  Download this image</a>
+      download="{{ $record['processed']['original']['location'] }}"><i class="fas fa-download mr-2"></i>  Download this image</a>
     </span>
     <span class="btn-wine btn m-1 p-2 share">
       <a href="/id/object/{{ $object['identifier'][1]['priref']}}">Back to record</a>
