3fe56efe10902dcabde7336a52ad9659da58e6b9
Daniel Pett <dejp3@cam.ac.uk>
2021-03-25 20:02:26 +0000

    Add image meta data

--- resources/views/includes/elements/identification.blade.php
+++ resources/views/includes/elements/identification.blade.php
@@ -5,7 +5,7 @@
       @if($id['type'] === 'uri')
         <li class="sr-only"><a href="{{ $id['value']}}">Stable URI</a></li>
       @elseif($id['type'] === 'priref')
-        <li>Primary reference Number: <a href="{{ env(COLLECTION_URL) }}/id/object/{{ $id['value']}}">{{ $id['value']}}</a></li>
+        <li>Primary reference Number: <a href="/id/object/{{ $id['value']}}">{{ $id['value']}}</a></li>
       @elseif($id['type'] === 'Online 3D model')
         <li class="sr-only"><a href="https://sketchfab.com/3d-models/{{ $id['value']}}">Sketchfab model</a></li>
       @else
--- resources/views/record/iiif.blade.php
+++ resources/views/record/iiif.blade.php
@@ -1,4 +1,13 @@
 @extends('layouts.iiif')
+@section('title', 'A IIIF Universal view of ' . ucfirst($object['title'][0]['value'] . ' : ' . $object['identifier'][0]['accession_number'] ))
+@section('description')
+  A IIIF Universal view of  {{ ucfirst($object['title'][0]['value']) }} : {{  $object['identifier'][0]['accession_number'] }}
+@endsection
+@if(array_key_exists('multimedia', $object))
+@section('hero_image', env('APP_URL') . '/imagestore/' . $object['multimedia'][0]['processed']['original']['location'])
+@else
+@section('hero_image','https://fitz-cms-images.s3.eu-west-2.amazonaws.com/img_20190105_153947.jpg')
+@endif
 @section('iiif')
-  @include('includes/structure/iiif')
+@include('includes/structure/iiif')
 @endsection
