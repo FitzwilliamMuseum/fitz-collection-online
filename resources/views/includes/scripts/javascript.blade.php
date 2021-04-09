e9af4cfe57f1dd5ed502c8c87969403e49c96d17
Daniel Pett <dejp3@cam.ac.uk>
2021-04-10 00:56:07 +0100

    Remove ref to material.js

--- resources/views/includes/scripts/javascript.blade.php
+++ resources/views/includes/scripts/javascript.blade.php
@@ -5,7 +5,6 @@
   crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script><!-- Back to top script -->
-<script src="/js/material.min.js"></script>
 <!-- Back to top script -->
 <script src="/js/backtotop.js"></script>
 <!-- lightbox -->
