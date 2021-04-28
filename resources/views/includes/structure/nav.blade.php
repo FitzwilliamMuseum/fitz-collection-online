75354933f24b886377633eb30409734eade26b47
Daniel Pett <dejp3@cam.ac.uk>
2021-04-28 23:33:35 +0100

    Amend nav

--- resources/views/includes/structure/nav.blade.php
+++ resources/views/includes/structure/nav.blade.php
@@ -12,9 +12,9 @@
     </li>
 
     <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownAbout" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
         About</a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownAbout">
           <a class="dropdown-item" href="{{ URL::to('https://beta.fitz.ms/about-us') }}">About the Museum</a>
           <a class="dropdown-item" href="{{ URL::to('https://beta.fitz.ms/about-us/collections') }}">Collection areas</a>
           <a class="dropdown-item" href="{{ URL::to('https://beta.fitz.ms/about-us/departments') }}">Departments</a>
@@ -27,8 +27,8 @@
         <a class="nav-link" href="https://tickets.museums.cam.ac.uk">Events and tickets</a>
       </li>
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Visit</a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownVisit" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Visit</a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownVisit">
           <a class="dropdown-item" href="https://beta.fitz.ms/visit-us">Your visit</a>
           <a class="dropdown-item" href="https://beta.fitz.ms/visit-us/exhibitions">Exhibitions</a>
           <a class="dropdown-item" href="https://beta.fitz.ms/visit-us/galleries">Galleries</a>
@@ -36,9 +36,9 @@
       </li>
 
       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownCollections" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           Collections</a>
-          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownCollections">
             <a class="dropdown-item" href="{{ URL::to('https://beta.fitz.ms/objects-and-artworks/') }}">An introduction</a>
             <a class="dropdown-item" href="{{ URL('https://collection.beta.fitz.ms') }}">Search our collection</a>
             <a class="dropdown-item" href="{{ URL::to('https://beta.fitz.ms/objects-and-artworks/highlights/') }}">Collection highlights</a>
@@ -46,9 +46,9 @@
           </div>
         </li>
         <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownLearning" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
             Learning</a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <div class="dropdown-menu" aria-labelledby="navbarDropdownLearning">
               <a class="dropdown-item" href="{{ URL::to('https://beta.fitz.ms/learning') }}">Learning</a>
               <a class="dropdown-item" href="{{ URL::to('https://beta.fitz.ms/learning/families') }}">Families</a>
               <a class="dropdown-item" href="{{ URL::to('https://beta.fitz.ms/learning/young-people') }}">Young people</a>
@@ -63,9 +63,9 @@
             <a class="nav-link" href="{{ URL::to('https://beta.fitz.ms/support-us') }}">Support</a>
           </li>
           <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownResearch" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Our research</a>
-              <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <div class="dropdown-menu" aria-labelledby="navbarDropdownResearch">
                 <a class="dropdown-item" href="{{ URL::to('https://beta.fitz.ms/research') }}">Research at the museum</a>
                 <a class="dropdown-item" href="{{ URL::to('https://beta.fitz.ms/research/projects') }}">Research projects</a>
                 <a class="dropdown-item" href="{{ URL::to('https://beta.fitz.ms/research/staff-profiles') }}">Researcher profiles</a>
