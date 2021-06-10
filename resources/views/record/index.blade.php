db22fe87b28b87b3f46dc485d555a8b90984aea1
Daniel Pett <dejp3@cam.ac.uk>
2021-06-10 13:39:18 +0100

    Add stack link

--- resources/views/record/index.blade.php
+++ resources/views/record/index.blade.php
@@ -24,6 +24,7 @@
             @include('includes/elements/name')
 
             @include('includes/elements/measurements')
+            {{-- https://stackoverflow.com/a/44242233 --}}
             <div class="text-center">
               <button type="button" class="btn btn-dark btn-circle btn-xl" data-toggle="collapse" data-target="#expand-more" aria-expanded="false" aria-controls="expand-more">
                 <span class="collapsed">
