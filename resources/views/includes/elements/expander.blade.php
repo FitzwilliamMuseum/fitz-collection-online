4abc163db08d531c0ae942ff7eeb81a8848a8604
Daniel Pett <dejp3@cam.ac.uk>
2021-06-11 23:00:47 +0100

    Change expander

--- resources/views/includes/elements/expander.blade.php
+++ resources/views/includes/elements/expander.blade.php
@@ -2,10 +2,10 @@
 <div class="text-center">
   <button type="button" class="btn btn-dark btn-circle btn-xl mb-5" data-toggle="collapse" data-target="#expand-more" aria-expanded="false" aria-controls="expand-more">
     <span class="collapsed">
-      Read More<br />@fa('plus')
+      @fa('plus')
     </span>
     <span class="expanded">
-      Read Less<br />@fa('minus')
+      @fa('minus')
     </span>
   </button>
 </div>
