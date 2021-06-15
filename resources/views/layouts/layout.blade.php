f573d08d086c18f66d6d1a2ef7a400780b924b1f
Daniel Pett <dejp3@cam.ac.uk>
2021-06-15 07:31:00 +0100

    manually add maps

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,7 +7,8 @@
     @include('includes.css.css')
 
     @hasSection('map')
-      @mapstyles
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css" integrity="sha512-xwE/Az9zrjBIphAcBb3F6JVqxf46+CDLwfLMHloNu6KEQCAWi6HcDUbeOfBIptF7tcCzusKFjFw2yuvEpDL9wQ==" crossorigin="" type="text/css">
+      <link rel="stylesheet" href="http://localhost:8000/vendor/maps/css/index.css?id=81569dd7736e102f4342" type="text/css">
     @endif
 
     @include('includes.structure.manifest')
@@ -79,7 +80,8 @@
   @endif
 
   @hasSection('map')
-    @mapscripts
+    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js" integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew==" crossorigin="" type="text/javascript"></script>
+    <script src="http://localhost:8000/vendor/maps/js/index.js?id=1e6f34e45ce1f8e9666f" type="text/javascript"></script>
     @include('includes.scripts.mapjs')
   @endif
 
