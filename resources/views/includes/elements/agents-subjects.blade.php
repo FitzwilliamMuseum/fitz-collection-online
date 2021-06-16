58dec961c88da9977acafd53f2e7917b3b9ecdef
Daniel Pett <dejp3@cam.ac.uk>
2021-06-16 10:30:01 +0100

    Moved names

--- resources/views/includes/elements/agents-subjects.blade.php
+++ resources/views/includes/elements/agents-subjects.blade.php
@@ -1,28 +1,36 @@
 
-  @if(array_key_exists('agents', $record['_source']))
+@if(array_key_exists('agents', $record['_source']))
   <h3 class="lead collection">
     People, subjects and objects depicted
   </h4>
   <ul class="entities">
-  @foreach($record['_source']['agents'] as $agent)
-    @if(array_key_exists('admin', $agent))
-      <li>
-        <a class="btn btn-sm btn-outline-dark mb-1" href="/id/agent/{{ $agent['admin']['id']}}">{{ ucfirst($agent['summary_title'])}}</a>
-      </li>
-    @else
-      <li>
-        {{ ucfirst($agent['summary_title'])}}
-      </li>
+
+    @if(array_key_exists('name', $record['_source']))
+
+      @foreach ($record['_source']['name'] as $name)
+        @if(array_key_exists('reference', $name))
+          <li>
+            <a class="btn btn-sm btn-outline-dark " href="{{ URL::to('/id/terminology/' . $name['reference']['admin']['id']) }}">{{ ucfirst($name['reference']['summary_title']) }}</a>
+          </li>
+        @else
+          <li>
+            <a class="btn btn-sm btn-outline-dark " href="#">{{ ucfirst($name['value']) }}</a></li>
+          </li>
+        @endif
+      @endforeach
+
     @endif
-  @endforeach
-  @foreach($record['_source']['subjects'] as $subject)
-    @if(array_key_exists('admin', $subject))
-      <li>
-        <a class="btn btn-sm btn-outline-dark mb-1" href="/id/terminology/{{ $subject['admin']['id']}}">{{ ucfirst($subject['summary_title'])}}</a>
-      </li>
-    @endif
-  @endforeach
-  @if(array_key_exists('subjects', $record['_source']))
+    @foreach($record['_source']['agents'] as $agent)
+      @if(array_key_exists('admin', $agent))
+        <li>
+          <a class="btn btn-sm btn-outline-dark mb-1" href="/id/agent/{{ $agent['admin']['id']}}">{{ ucfirst($agent['summary_title'])}}</a>
+        </li>
+      @else
+        <li>
+          {{ ucfirst($agent['summary_title'])}}
+        </li>
+      @endif
+    @endforeach
     @foreach($record['_source']['subjects'] as $subject)
       @if(array_key_exists('admin', $subject))
         <li>
@@ -30,6 +38,14 @@
         </li>
       @endif
     @endforeach
-  @endif
+    @if(array_key_exists('subjects', $record['_source']))
+      @foreach($record['_source']['subjects'] as $subject)
+        @if(array_key_exists('admin', $subject))
+          <li>
+            <a class="btn btn-sm btn-outline-dark mb-1" href="/id/terminology/{{ $subject['admin']['id']}}">{{ ucfirst($subject['summary_title'])}}</a>
+          </li>
+        @endif
+      @endforeach
+    @endif
   </ul>
-  @endif
+@endif
