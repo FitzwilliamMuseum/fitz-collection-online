e347faf9f1164fecb967e374ef6522059b2827f6
Daniel Pett <dejp3@cam.ac.uk>
2021-10-05 18:59:48 +0100

    Change size

--- resources/views/record/qr.blade.php
+++ resources/views/record/qr.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container-fluid">
   <div class="text-center">
-    <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(800)->generate($data['admin']['uri'])) !!}" class="img-fluid">
+    <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(500)->generate($data['admin']['uri'])) !!}" class="img-fluid">
   </div>
 </div>
 @endsection
