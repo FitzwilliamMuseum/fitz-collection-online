c83aa100d9c4761855b4963312940a1fae793ba6
Daniel Pett <dejp3@cam.ac.uk>
2021-10-05 18:49:21 +0100

    Remove semi-colon

--- resources/views/record/qr.blade.php
+++ resources/views/record/qr.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.qr')
 @section('content')
 <div class="center-text">
-<img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(500)->margin(10)->generate($data['admin']['uri']);!!}" class="img-fluid">
+<img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(500)->margin(10)->generate($data['admin']['uri']) !!}" class="img-fluid">
 </div>
 @endsection
