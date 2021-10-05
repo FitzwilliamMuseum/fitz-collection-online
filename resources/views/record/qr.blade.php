9c6c0d9cdc8763ea520160e72617ea2debc47943
Daniel Pett <dejp3@cam.ac.uk>
2021-10-05 18:55:02 +0100

    Center image

--- resources/views/record/qr.blade.php
+++ resources/views/record/qr.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.qr')
 @section('content')
 <div class="container-fluid">
-  <div class="center-text">
+  <div class="text-center">
     <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(500)->margin(10)->generate($data['admin']['uri'])) !!}" class="img-fluid">
   </div>
 </div>
