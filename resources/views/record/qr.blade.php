d7e2fdaab8f4e949d916a091a5b8319ea1da832f
Daniel Pett <dejp3@cam.ac.uk>
2021-10-05 19:01:03 +0100

    Add padding

--- resources/views/record/qr.blade.php
+++ resources/views/record/qr.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="container-fluid">
   <div class="text-center">
-    <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(500)->generate($data['admin']['uri'])) !!}" class="img-fluid">
+    <img src="data:image/png;base64, {!! base64_encode(QrCode::format('png')->size(500)->generate($data['admin']['uri'])) !!}" class="py-5 img-fluid">
   </div>
 </div>
 @endsection
