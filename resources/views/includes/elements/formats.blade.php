6b77baa7ac714cb0c8b8bc5278032a9495509a8c
Daniel Pett <dejp3@cam.ac.uk>
2021-10-05 15:07:22 +0100

    Add qr

--- resources/views/includes/elements/formats.blade.php
+++ resources/views/includes/elements/formats.blade.php
@@ -2,5 +2,5 @@
   <btn class="btn btn-wine m-1"><a href="{{ url()->current() }}/json">json</a></btn>
   <btn class="btn btn-wine m-1"><a href="{{ url()->current() }}/xml">xml</a></btn>
   <btn class="btn btn-wine m-1"><a href="{{ url()->current() }}/txt">txt</a></btn>
-  <btn class="btn btn-wine m-1"><a href="{{ url()->current() }}/qr">txt</a></btn>
+  <btn class="btn btn-wine m-1"><a href="{{ url()->current() }}/qr">QR code</a></btn>
 </div>
