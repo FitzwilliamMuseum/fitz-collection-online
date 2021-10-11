86280088b8de2d11143a40e4770b3d3034271fdb
Daniel Pett <dejp3@cam.ac.uk>
2021-10-11 15:50:13 +0100

    Amend size of box

--- resources/views/includes/elements/measurements.blade.php
+++ resources/views/includes/elements/measurements.blade.php
@@ -1,68 +1,73 @@
 @if(array_key_exists('measurements', $record['_source']))
 
 
-<div class="row">
+  <div class="row">
 
 
-<div class="col-md-6">
-  <h3 class="lead collection">
-    Measurements and weight
-  </h3>
-<p>
-  @foreach($record['_source']['measurements']['dimensions'] as $dim)
-    @if(array_key_exists('dimension',$dim) && array_key_exists('units',$dim))
-      {{ $dim['dimension'] }}: {{ $dim['value'] }} {{ $dim['units'] }}<br/>
-    @else
-      @section('dims-message')
-          <p class="text-info">
-            At the moment, this record does not display units or type of
-            measurements. We will rectify this as soon as possible.
-          </p>
-      @endsection
-        {{ $dim['value'] }}<br/>
-    @endif
-  @endforeach
-</p>
-</div>
-@php
-$dimensions = $record['_source']['measurements']['dimensions'];
-$dims = [];
-foreach($dimensions as $dim){
-
-  if($dim['dimension'] == 'Height')
-  {
-    $dims['height'] = $dim['value'];
-  }
-  if($dim['dimension'] == 'Width'){
-    $dims['width'] = $dim['value'];
-  }
-  if($dim['dimension'] == 'Depth'){
-    $dims['depth'] = $dim['value'];
-  }
-  if($dim['dimension'] == 'Thickness'){
-    $dims['depth'] = $dim['value'];
-  }
-  if($dim['dimension'] == 'Length'){
-    $dims['width'] = $dim['value'];
-  }
-}
-@endphp
-@if(array_key_exists('height', $dims) && array_key_exists('width', $dims))
-  @php
-  if(!array_key_exists('depth', $dims)){
-    $dims['depth'] = 0.01;
-  }
+    <div class="col-md-6">
+      <h3 class="lead collection">
+        Measurements and weight
+      </h3>
+      <p>
+        @foreach($record['_source']['measurements']['dimensions'] as $dim)
+          @if(array_key_exists('dimension',$dim) && array_key_exists('units',$dim))
+            {{ $dim['dimension'] }}: {{ $dim['value'] }} {{ $dim['units'] }}<br/>
+          @else
+            @section('dims-message')
+              <p class="text-info">
+                At the moment, this record does not display units or type of
+                measurements. We will rectify this as soon as possible.
+              </p>
+            @endsection
+            {{ $dim['value'] }}<br/>
+          @endif
+        @endforeach
+      </p>
+    </div>
+    @php
+    $dimensions = $record['_source']['measurements']['dimensions'];
+    $dims = [];
+    $width = [];
+    $depth = [];
+    foreach($dimensions as $dim){
+      if($dim['dimension'] == 'Height')
+      {
+        $dims['height'] = $dim['value'];
+      }
+      if($dim['dimension'] == 'Width'){
+        array_push($width, $dim['value']);
+      }
+      if($dim['dimension'] == 'Length'){
+        array_push($width, $dim['value']);
+      }
+      if($dim['dimension'] == 'Depth'){
+        array_push($depth, $dim['value']);
+      }
+      if($dim['dimension'] == 'Thickness'){
+        array_push($depth, $dim['value']);
+      }
+    }
+    $w = max($width);
+    if(count($depth) < 2){
+      $d = min($width);
+    } elseif(count($depth) > 2) {
+      $d = max($depth);
+    } else {
+      $d = 0.01;
+    }
   @endphp
-  <x-Dimension-Drawer
-  :height="$dims['height']"
-  :width="$dims['width']"
-  :depth="$dims['depth']"
-  :units="$record['_source']['measurements']['dimensions'][0]['units']"
-  :viewWidth="400"
-  :viewHeight="320"
-  :scale=1
-  />
-@endif
+
+  @if(array_key_exists('height', $dims) && !empty($w))
+    <x-Dimension-Drawer
+    :height="$dims['height']"
+    :width="$w"
+    :depth="$d"
+    :units="$record['_source']['measurements']['dimensions'][0]['units']"
+    :viewWidth="400"
+    :viewHeight="320"
+    :scale=1
+    />
+  @endif
 @endif
 @yield('dims-message')
 </div>
