e3a1af4d217daa200aa9b7aac1ea48f8e500a70f
Daniel Pett <dejp3@cam.ac.uk>
2022-04-05 10:30:05 +0100

    Upgrade packages

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/id/object/2002');
 
         $response->assertStatus(200);
     }
