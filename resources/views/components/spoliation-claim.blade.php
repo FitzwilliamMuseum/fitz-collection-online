1b48d9fa9f458f023cb8a4a11c6a5f169c876c6f
Daniel Pett <dejp3@cam.ac.uk>
2022-04-06 14:14:34 +0100

    Add quote

--- resources/views/components/spoliation-claim.blade.php
+++ resources/views/components/spoliation-claim.blade.php
@@ -4,7 +4,7 @@
             <div class="alert alert-info text-dark text-center w-50 mx-auto">
                 @foreach($spoliation as $message)
                     <p>{{ $message['text']  }}</p>
-                    <a class="btn btn-dark" href={{ env("MAIN_URL") }}/news/{{ $message['news_slug'] }}"
+                    <a class="btn btn-dark" href="{{ env("MAIN_URL") }}/news/{{ $message['news_slug'] }}"
                        title="Full details for this case">Read the details for this case</a>
                 @endforeach
             </div>
