2014ea348ba937f2f8ff611d7f4f13d673f21449
Daniel Pett <dejp3@cam.ac.uk>
2022-05-05 11:23:18 +0100

    Change middleware

--- app/Http/Middleware/SecureHeadersMiddleware.php
+++ app/Http/Middleware/SecureHeadersMiddleware.php
@@ -22,7 +22,7 @@ class SecureHeadersMiddleware
       $this->removeUnwantedHeaders($this->unwantedHeaderList);
       $response = $next($request);
       $response->headers->set('Referrer-Policy', 'no-referrer-when-downgrade');
-      $response->headers->set('X-Content-Type-Options', 'nosniff');
+//      $response->headers->set('X-Content-Type-Options', 'nosniff');
       $response->headers->set('X-XSS-Protection', '1; mode=block');
       $response->headers->set('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
       return $response;
