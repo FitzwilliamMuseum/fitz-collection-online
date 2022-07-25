eff8761a0186eb43b6e7538bf0a7fd01cb387e05
Daniel Pett <dejp3@cam.ac.uk>
2022-07-25 21:21:37 +0100

    Fix rendering of data for location

--- resources/views/components/display-status.blade.php
+++ resources/views/components/display-status.blade.php
@@ -1,14 +1,21 @@
-@if(is_array($location->adlibJSON->recordList->record))
-    <div class="mb-2 text-center">
+@if(property_exists($location->adlibJSON,'recordList'))
+    @if(is_array($location->adlibJSON->recordList->record))
+    @foreach($location->adlibJSON->recordList->record as $record)
+        @if(property_exists($record, "current_location"))
+            <div class="mb-2 text-center">
         <span class="btn-outline-dark btn">
-        @foreach($location->adlibJSON->recordList->record as $record)
-                @if($record->{"current_location.type"}[0] != 'display')
-                    Current Location: In storage
-                @else
-                    Current Location: {{$record->{"current_location.description"}[0]}}
-                @endif
-            @endforeach
-    </span>
-    </div>
+            @if($record->{"current_location.type"}[0] != 'display')
+                Current Location: In storage
+            @else
+                Current Location: {{$record->{"current_location.description"}[0]}}
+            @endif
+        </span>
+            </div>
+        @else
+            <div class="mb-2 text-center">
+                <span class="btn-outline-dark btn">Awaiting location update</span>
+            </div>
+        @endif
+    @endforeach
 @endif
-
+@endif
