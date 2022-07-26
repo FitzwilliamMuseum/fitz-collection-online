fa46fe62c4c09c5a04d12069b86f0d0a9f194da1
Daniel Pett <dejp3@cam.ac.uk>
2022-07-26 12:55:21 +0100

    Change btn-wine

--- resources/views/includes/elements/formats.blade.php
+++ resources/views/includes/elements/formats.blade.php
@@ -1,6 +1,6 @@
 <div class="share">
-    <button class="btn btn-wine m-1"><a href="{{ url()->current() }}/json">json</a></button>
-    <button class="btn btn-wine m-1"><a href="{{ url()->current() }}/xml">xml</a></button>
-    <button class="btn btn-wine m-1"><a href="{{ url()->current() }}/txt">txt</a></button>
-    <button class="btn btn-wine m-1"><a href="{{ url()->current() }}/qr">QR code</a></button>
+    <button class="btn btn-dark m-1" aria-label="View this object as a JSON file"><a href="{{ url()->current() }}/json">json</a></button>
+    <button class="btn btn-dark m-1" aria-label="View this object as XML"><a href="{{ url()->current() }}/xml">xml</a></button>
+    <button class="btn btn-dark m-1" aria-label="View this object as a text file"><a href="{{ url()->current() }}/txt">txt</a></button>
+    <button class="btn btn-dark m-1" aria-label="Access a QR code for this object"><a href="{{ url()->current() }}/qr">QR code</a></button>
 </div>
