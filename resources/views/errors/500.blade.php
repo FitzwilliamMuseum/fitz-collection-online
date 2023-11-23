2af5ef336b77c33f54bdca822fc7811c846000cc
Alan Isaacson <38862429+AlanJIsaacson@users.noreply.github.com>
2023-11-23 14:58:10 +0000

    Update 500.blade.php

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -3,14 +3,28 @@
 @section('hero_image','https://fitz-cms-images.s3.eu-west-2.amazonaws.com/cupidpsychelarge.jpg')
 @section('hero_image_title', 'Cupid and Psyche - del Sallaio')
 @section('content')
-    <h2 class="lead">{{$exception->getStatusCode()}} Error</h2>
+    <h2 class="lead">Internal server error {{$exception->getStatusCode()}}</h2>
 
-    <div class="col-12 shadow-sm p-3 mx-auto mb-3 rounded ">
-        <figure class="figure">
-            <img class="img-fluid" src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/searle_cat.jpg"
-                 alt="Searle's grumpy cat"/>
-        </figure>
-        <p>Sorry, we have a problem with our code. This problem has been noted and,
-            we will see if we can fix it.</p>
+<div class="col-12 shadow-sm p-3 mx-auto mb-3 mt-3">
+    <div class="row">
+        <div class="col-md-6">
+            <figure class="figure">
+                <img alt="An image of a very grumpy cat"
+                     class="img-fluid"
+                     width="416"
+                     height="416"
+                     src="https://fitz-cms-images.s3.eu-west-2.amazonaws.com/searle_cat.jpg"
+                />
+                <figcaption class="figure-caption">
+                    One of Ronald Searle's cats, given to the Fitzwilliam Museum in 2014 by his family.
+                </figcaption>
+            </figure>
+        </div>
+        <div class="col-md-6">
+            <p>Sorry, we have a problem with our code. This problem has been noted and,
+                we will see if we can fix it.</p>
+        </div>
     </div>
+</div>
 @endsection
+
